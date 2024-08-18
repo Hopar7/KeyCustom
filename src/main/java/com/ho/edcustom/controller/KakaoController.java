18aedf5ab5c7f70424fc01b566670fe10d3a4e22
Hopar7 <mkiu7030@naver.com>
2024-08-18 17:37:05 +0900

    주석 정리
    주석정리

--- src/main/java/com/ho/edcustom/controller/KakaoController.java
+++ src/main/java/com/ho/edcustom/controller/KakaoController.java
@@ -19,14 +19,6 @@ public class KakaoController {
         LoginResponse Response = new LoginResponse(kakaoService.kakaoLogin(DTO.getCode()));
         return Response;
     }
-
-//@PostMapping("/user/kakao/login")
-//public LoginResponseDto kakaoLogin(@RequestParam String code) throws JsonProcessingException {
-//    LoginResponseDto DTO = new LoginResponseDto(kakaoService.kakaoLogin(code));
-//    return DTO;
-//    }
-//    //500에러 - 디버그 400에러
-//    //근데 리다이랙트 엔드포인트 call back으로 바꾸면 200 성공함 왜인지 알아야함.
-//    //Post,Get 일단 둘다됨
+    
 }
 
