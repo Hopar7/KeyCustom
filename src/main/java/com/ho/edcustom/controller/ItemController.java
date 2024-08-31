8fc708741a2d860187a0d2a7d5506adf89944584
mkiu7030 <mkiu7030@naver.com>
2024-08-31 16:35:30 +0900

    아이템 컨트롤러 오류 수정

--- src/main/java/com/ho/edcustom/controller/ItemController.java
+++ src/main/java/com/ho/edcustom/controller/ItemController.java
@@ -20,7 +20,7 @@ public class ItemController {
     @PostMapping("/items/save")
     public void itemSave(@RequestBody ItemRequest DTO)
     {
-        itemService.saveItem(DTO.getEmail(),DTO.getSavetime(),DTO.getBarebonecolor(),DTO.getKeyboardtype(),DTO.getKeyboardtype(),DTO.getDesign(),DTO.getSwitchcolor());
+        itemService.saveItem(DTO.getEmail(),DTO.getSavetime(),DTO.getBarebonecolor(),DTO.getKeyboardtype(),DTO.getKeycapcolor(),DTO.getDesign(),DTO.getSwitchcolor());
     }
     @PostMapping("/items/find")
     public ResponseEntity<List<Item>> itemFind(@RequestBody EmailRequest DTO)
--- src/main/java/com/ho/edcustom/repository/ItemRepository.java
+++ src/main/java/com/ho/edcustom/repository/ItemRepository.java
@@ -1,11 +1,9 @@
 package com.ho.edcustom.repository;
 
 import com.ho.edcustom.entity.Item;
-import com.ho.edcustom.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ItemRepository extends JpaRepository<Item,Long> {
     List<Item> findItemByEmail(String email);
